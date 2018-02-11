1dad915a6b0b7bf03ec4f21306dfa3020e26002b
John Wattenbarger <johncwattenbarger@gmail.com>
2018-02-11 08:21:58 -0600

    main() program for Solubility Rules

---
+++ src/solubility_rules/Solubility_Rules.java
@@ -0,0 +1,10 @@
+package solubility_rules;
+public class Solubility_Rules {
+
+    public static void main(String[] args) 
+    {
+        SFrame x = new SFrame();
+        x.setVisible(true);
+    }
+    
+}
